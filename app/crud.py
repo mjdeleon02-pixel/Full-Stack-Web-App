3997a345874dfdb18229939a5c43a72635794d2a
mjdeleon02-pixel <mjdeleon02@tip.edu.ph>
2026-03-01 16:43:01 +0800

    Update crud.py 1

--- app/crud.py
+++ app/crud.py
@@ -3,12 +3,13 @@ import os
 from bson import ObjectId
 
 def init_db():
-    uri = os.environ.get('MONGO_URI')
+    uri = os.environ.get('MONGO_URI')  # Get the connection URI from the environment
     client = MongoClient(uri)
-    return client["flaskappdocker"]
+    db_name = os.environ.get('MONGO_DB', 'flaskappdocker')  # Default DB name if not set
+    return client[db_name]
 
 def get_items(db):
-    return [ {"_id": str(doc['_id']), "name": doc['name']} for doc in db.items.find() ]
+    return [{"_id": str(doc['_id']), "name": doc['name']} for doc in db.items.find()]
 
 def add_item(db, data):
     result = db.items.insert_one({"name": data['name']})
